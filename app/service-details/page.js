047d1d210f92ce0678daf3533331fc5805155ef6
AL1Skey <adamnurramadan@gmail.com>
2024-10-02 09:42:06 +0700

    chore: Update service details page with job vacancy information

--- app/service-details/page.js
+++ app/service-details/page.js
@@ -6,6 +6,41 @@ import AboutSlider1 from "@/components/slider/AboutSlider1"
 import Link from 'next/link'
 
 import { useState } from 'react'
+
+const textTemplate = `
+                                            PT. Topaz Maritime
+
+Open job vacancy for crew as follow:
+
+ 
+
+Rank: Bosun-Pumpman-Fitter-Cook
+
+Type vessel: Bulk Carier and Chemical Tanker
+
+Trading area: Foreign Going
+
+ 
+
+Requirements:
+
+- Experience Minimum 2 years’ experience on the same ship type
+
+- Have experience onboard with foreign crew
+
+- Have experience onboard trading area worldwide
+
+- Documents and Certificate minimum valid min 1 year
+
+- Completed Vaccine Covid 19
+
+- Must be fluent in English
+
+ 
+
+If you meet criteria or know who interest vacancy might be above, please send CV to email recruitmenttopaz@pttm.co.id
+`
+
 export default function Page() {
     const [isActive, setIsActive] = useState({
         status: false,
@@ -29,53 +64,58 @@ export default function Page() {
         <>
             <Layout headerStyle={1} footerStyle={1} breadcrumbTitle="Karir">
                 <div>
-                    
+
                     {/*=====service details box end=======*/}
                     {/*=====service details binifet end=======*/}
-                    <div className="">
-                        <div className="p-10">
-                            <div className="row align-items-center">
+                    <div className="service-details-binifite sp2 page-bg">
+                        <div className="container">
+                        {[1,2,3,4,5,6,7].map((value,index)=>(
+                            <div key={index} className="row align-items-center w-10!">
+                                <div className="space100" />
                                 <div className="col-md-6">
                                     <div className="apartment-imgs">
-                                        <div className="apartment-img1">
-                                            <img src="/assets/img/shapes/apartment-img-1.svg" alt="" />
-                                        </div>
                                         <div className="apartment-img2 border15">
                                             <img src="/assets/img/image/service-details1.png" alt="" />
-                                        </div>
-                                        <div className="apartment-img3">
-                                            <img src="/assets/img/shapes/apartment-img-3.svg" alt="" />
                                         </div>
                                     </div>
                                 </div>
                                 <div className="col-md-6">
                                     <div className="hadding2">
+                                        {/* Heading Title */}
+                                        <h4 className="font-f-2 font-22 line--height-22 weight-500"> <Link href="#">Berakhir Pada 29 October, 2023</Link></h4>
+                                        <div className="space24" />
+                                        {/* Title */}
                                         <h1 className="font-f-2 weight-700 font-32 font-lg-44 line-height-38 line-height-lg-44">
-                                            Benefits you will get in our business <span className="after">consultaing</span>
+                                        BOSUN-PUMPMAN-FITTER
+                                        {/* <span className="after">consultaing</span> */}
                                         </h1>
+                                        {/* Description */}
                                         <div className="space24" />
-                                        <p className="font-16 font-f-2 line-height-p-30 font-18 weight-400">
-                                            By choosing our services, you not only save on costs but also gain access to a team of experienced managers who are solely dedicated to driving revenue growth for your startup.
+                                        {textTemplate.split("\n").map((text, index) => (
+                                        <p key={index} className="font-16 font-f-2 line-height-p-30 font-18 weight-400">
+                                            {text}
+                                            <br />
                                         </p>
-                                        <div className="space24" />
-                                        <h4 className="font-f-2 font-22 line--height-22 weight-500"> <Link href="#">Listening your needs for IT Strategy</Link></h4>
+                                        ))}
+{/*                                         
                                         <div className="space16" />
                                         <p className="font-f-2 weight-400 line-height-28 font-16">We won’t just tell you what to do; we will show the ‘how to’ execute the proven strategies and systems.</p>
                                         <div className="space32" />
                                         <h4 className="font-f-2 font-22 line--height-22 weight-500"> <Link href="#">Solutions Taliored to your needs</Link></h4>
                                         <div className="space16" />
-                                        <p className="font-f-2 weight-400 line-height-28 font-16">If you do the work, your investment in coaching will more than 10 times pay for itself in financial returns.</p>
+                                        <p className="font-f-2 weight-400 line-height-28 font-16">If you do the work, your investment in coaching will more than 10 times pay for itself in financial returns.</p> */}
                                         <div className="space40" />
                                         <div className="home2-btn">
-                                            <Link className="font-18 line-height-30 font-f-2 font-w" href="#">Find Apartment</Link>
+                                            <Link className="font-18 line-height-30 font-f-2 font-w" href="mailto:adamnurramadan@gmail.com">Send Email</Link>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                        ))}
                         </div>
                     </div>
                     {/*=====service details binifet end=======*/}
-                    
+
                 </div>
 
             </Layout>
